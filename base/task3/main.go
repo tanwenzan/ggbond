7e6a95d0d2906c9bb8d819bd50a5c155b121b550
tanwenzan <2011277648@qq.com>
2025-04-13 22:44:38 +0800

    feat: 提交第三次作业

--- base/task3/main.go
+++ base/task3/main.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 func main() {
 	// sqlx
 	QueryByDepart()
@@ -7,4 +9,18 @@ func main() {
 
 	// books
 	QueryByBook()
+
+	post, err := GetMostCommentedPost()
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println(post)
+	}
+
+	comments, err := GetUserPostsWithComments(1)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println(comments)
+	}
 }
